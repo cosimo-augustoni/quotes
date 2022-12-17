f0cea66d6a590dcb9de56315875c21eec8577803
Cosimo Augustoni <cosimo.augustoni@gmail.com>
2022-12-17 13:57:21 +0100

    Import Funktion für Quotes 1.0 Daten

---
+++ src/quotes/quotes-web/Data/Import/ImportContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace quotes_web.Data.Import
+{
+    public class ImportContainer
+    {
+        public List<Author>? Authors { get; set; }
+
+        public List<Quote>? Quotes { get; set; }
+    }
+}
