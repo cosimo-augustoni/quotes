5df7b45173a08f2f6d82ad7dec70f383a3d35cb5
Joan Künzler <joan.kuenzler@gmail.com>
2022-12-17 18:25:55 +0100

    Refactor to own components

---
+++ src/quotes/quotes-web/Pages/AuthorCreation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace quotes_web.Pages
+{
+    public class AuthorCreation
+    {
+        [Required]
+        [StringLength(255, ErrorMessage = "Name zu lang")]
+        public string? Name { get; set; }
+    }
+}
---
+++ src/quotes/quotes-web/Pages/QuoteCreation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace quotes_web.Pages
+{
+    public class QuoteCreation
+    {
+        [Required]
+        [StringLength(1024,
+        ErrorMessage = "Text zu lang")]
+        public string? Text { get; set; }
+        [Required]
+        public Guid? AuthorId { get; set; }
+    }
+}
