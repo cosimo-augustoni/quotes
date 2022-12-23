f8a8b69503f53e5c14854bac7f6f831bb711c0fe
Cosimo Augustoni <cosimo.augustoni@gmail.com>
2022-12-23 21:15:12 +0100

    Image Resizing

--- src/quotes/quotes-web/Domain/Quoting/Author/AuthorService.cs
+++ src/quotes/quotes-web/Domain/Quoting/Author/AuthorService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Imageflow.Fluent;
+using Microsoft.EntityFrameworkCore;
 using quotes_web.Persistence.Quoting;
 
 namespace quotes_web.Domain.Quoting.Author
@@ -28,9 +29,13 @@ namespace quotes_web.Domain.Quoting.Author
                 Name = authorCreation.Name,
                 FileId = file.Id,
             };
-
             await using var fileStream = System.IO.File.Create(file.FilePath);
-            await authorCreation.FileCreation.FileEntry.OpenReadStream(authorCreation.FileCreation.FileEntry.Size).CopyToAsync(fileStream);
+            using var job = new ImageJob();
+            await job.BuildCommandString(
+                new StreamSource(authorCreation.FileCreation.FileEntry.OpenReadStream(authorCreation.FileCreation.FileEntry.Size), false), 
+                new StreamDestination(fileStream, false), 
+                "width=600&height=600&mode=max&scale=down")
+                .Finish().InProcessAsync();
 
             await using var context = await this.dbContextFactory.CreateDbContextAsync();
             await context.AddAsync(file);
