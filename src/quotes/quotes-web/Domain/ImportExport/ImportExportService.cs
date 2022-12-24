9bd9df14c7425a7c004411190a5a90caba2c1ab7
Cosimo Augustoni <cosimo.augustoni@gmail.com>
2022-12-24 02:04:23 +0100

    Import Export fixes

--- src/quotes/quotes-web/Domain/ImportExport/ImportExportService.cs
+++ src/quotes/quotes-web/Domain/ImportExport/ImportExportService.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using quotes_web.Persistence.Quoting;
 using System.Text.Json;
+using File = quotes_web.Persistence.Quoting.File;
 
 namespace quotes_web.Domain.ImportExport
 {
@@ -21,9 +22,22 @@ namespace quotes_web.Domain.ImportExport
             stream.Seek(0, SeekOrigin.Begin);
 
             await using var context = await this.dbContextFactory.CreateDbContextAsync();
-            var importContainer = JsonSerializer.Deserialize<ImportExportContainer>(stream);
+            var importContainer = JsonSerializer.Deserialize<ImportContainer>(stream);
             if (importContainer?.Authors != null)
-                context.Authors.AddRange(importContainer.Authors);
+            {
+                foreach (var importAuthor in importContainer.Authors)
+                {
+                    var image = importAuthor.File ?? new File { FileType = "image/png", Id = Guid.NewGuid(), Name = "NoImage" };
+                    var author = new Author
+                    {
+                        Id = importAuthor.Id ?? throw new ArgumentException("Id is Null"),
+                        Name = importAuthor.Name ?? throw new ArgumentException("Name is Null"),
+                        File = image,
+                        FileId = image.Id
+                    };
+                    context.Authors.Add(author);
+                }
+            }
 
             if (importContainer?.Quotes != null)
                 context.Quotes.AddRange(importContainer.Quotes);
@@ -34,7 +48,7 @@ namespace quotes_web.Domain.ImportExport
         public async Task<Stream> ExportAsync()
         {
             await using var context = await this.dbContextFactory.CreateDbContextAsync();
-            var container =  new ImportExportContainer
+            var container =  new ExportContainer
             {
                 Authors = await context.Authors.Include(a => a.File).AsNoTracking().ToListAsync(),
                 Quotes = await context.Quotes.AsNoTracking().ToListAsync()
@@ -46,11 +60,29 @@ namespace quotes_web.Domain.ImportExport
             return stream;
         }
 
-        private class ImportExportContainer
+        private class ImportContainer
+        {
+            public List<ImportAuthor>? Authors { get; set; }
+
+            public List<Quote>? Quotes { get; set; }
+        }
+
+        private class ExportContainer
         {
             public List<Author>? Authors { get; set; }
 
             public List<Quote>? Quotes { get; set; }
+        }
+
+        private class ImportAuthor
+        {
+            public Guid? Id { get; init; }
+
+            public string? Name { get; init; }
+
+            public Guid? FileId { get; init; }
+
+            public  File? File { get; set; }
         }
     }
 }
